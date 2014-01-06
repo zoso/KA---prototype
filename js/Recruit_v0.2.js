f4cd0ea39f2899cd17a2304928e29ecea979ab29
Lars Lund <larslund@Larss-MacBook-Pro.local>
2014-01-06 15:54:14 +0100

    backup 06.01.14

--- js/Recruit_v0.2.js
+++ js/Recruit_v0.2.js
@@ -1,47 +1,23 @@
 var Recruit = {
-	container: '.jobs',
-	init: function(data) {
-		var lang_url = "#";
-		$.each( data, function( key, val ) {
-			if (key == 'positions') {
-				//Recruit.sort(val);		
-			}
-			if (key == 'langurl') {
-				lang_url = val;
-			}
-			if (key == 'lang') {
-				$.each(val, function(i, v) {
-					var str = '<div class="lang">';
-					str += '<a href="'+lang_url+v.shortname+'"><img src="'+v.img+'" />';
-					//str += '<p>'+v.name+'</p>';
-					str += '</a></div>';
-					$(Recruit.container).append(str);
-				});
-			}
-		});
-		var tab = "<table width='100%' cellspacing='0' cellpadding='0' border='0'>";
-		tab += "<thead><tr><td>Position title</td>";
-		tab += "<td>Department</td>";
-		tab += "<td>Location</td>";
-		tab += "<td>Closing date</td></tr></thead>";
-		$.each(data.positions, function(i, item) {
-			tab += "<tr>";
-			$.each(item, function(j, str) {
-				switch(j) {
-					case "position_title":
-						tab += "<td><a href='#'>"+str+"</a></td>";
-						break;
-					case "url":
-						//open where?
-						break;
-					default:
-						tab += "<td>"+str+"</td>";
-						break;		
-				}
-			});
-			tab += "</tr>";
-		});
-		tab += "</table>";
-		$(Recruit.container).append(tab);
-	}
+    container: 'jobs',
+    init: function (data) {
+
+        var cont = document.getElementById(this.container);
+        
+        var tab = '<table width="100%" cellspacing="0" cellpadding="0" border="0">';
+        tab += '<thead><tr><td>Position title</td>';
+        tab += '<td>Department</td>';
+        tab += '<td>Location</td>';
+        tab += '<td>Closing date</td></tr></thead>';
+        for (var i = 0; i < data.Positions.length; i++) {
+            tab += '<tr>';
+            tab += '<td><a href="' + data.Positions[i].Url + '">' + data.Positions[i].Title + '</a></td>';
+            tab += '<td>' + data.Positions[i].Department + '</td>';
+            tab += '<td>' + data.Positions[i].Location + '</td>';
+            tab += '<td>' + data.Positions[i].ClosingDate + '</td>';
+            tab += "</tr>";
+        }
+        tab += "</table>";
+       cont.innerHTML = tab;
+    }
 };
